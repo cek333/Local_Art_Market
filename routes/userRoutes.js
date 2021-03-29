c0f7144d11f3b94c90701e4d99a738e5d3a9f434
Cecil <69444825+cek333@users.noreply.github.com>
2021-03-29 14:14:45 -0400

    centralize construction of user object for client

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,14 +3,42 @@ const customAuth = require('../middleware/customPassportAuthenticate');
 const express = require('express');
 const router = express.Router();
 
+// const UsersDAO = require('../dao/UsersDAO');
+// router.post('/test_id', async function(req, res) {
+//   const result = await UsersDAO.getUserById(req.body.id);
+//   console.log('user_by_id', result);
+//   res.json({ status: true, message: "Test by Id Success!" });
+// });
+
+// router.post('/test_email', async function(req, res) {
+//   const email = req.body.email;
+//   const type = req.body.type;
+//   const result = await UsersDAO.getUserByEmail(email, type);
+//   console.log('user_by_email', result);
+//   res.json({ status: true, message: "Test by Email Success!" });
+// });
+
+function constructUserForClient(user) {
+  if (user) {
+    return {
+      id: user.typeId,
+      type: user.type,
+      name: user.name,
+      location: user.location
+    };
+  } else {
+    return { id: '', type: '', name: '', location: null };
+  }
+}
+
 // Using the passport.authenticate middleware with our local strategy.
 router.post('/login', customAuth, function(req, res) {
-  res.json({ status: true, id: req.user._id, type: req.user.type, message: "You're now logged in!" });
+  res.json({ status: true, ...constructUserForClient(req.user), message: "You're now logged in!" });
 });
 
 // Route for signing up a user.
 router.post('/signup', customSignup, customAuth, function(req, res) {
-  res.json({ status: true, id: req.user._id, type: req.user.type, message: "You're now logged in!" });
+  res.json({ status: true, ...constructUserForClient(req.user), message: "You're now logged in!" });
 });
 
 // Route for logging user out
@@ -28,11 +56,7 @@ router.post('/logout', function(req, res) {
 
 // Get current logged in user
 router.get('/fetch', function(req, res) {
-  if (req.user) {
-    res.json({ status: true, id: req.user._id, type: req.user.type });
-  } else {
-    res.json({ status: false, id: '', type: '' });
-  }
+  res.json({ status: true, ...constructUserForClient(req.user) });
 });
 
 module.exports = router;
