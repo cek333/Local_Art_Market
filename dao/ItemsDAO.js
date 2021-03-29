9c458e1cdef41c471f0bf146437cac72ee162a71
Cecil <69444825+cek333@users.noreply.github.com>
2021-03-29 12:00:05 -0400

    update item fields; add indexes on creation

--- dao/ItemsDAO.js
+++ dao/ItemsDAO.js
@@ -7,23 +7,45 @@ class ItemsDAO {
     if (items) {
       return;
     }
-    try {
-      items = await conn.db(process.env.MONGODB_DB).collection('items');
-    } catch (e) {
-      console.error(`Unable to connect to collection: ${e}`);
-    }
+    // Create collection + index immediately if it doesn't exist
+    conn.db(process.env.MONGODB_DB).collection('items', { strict: true }, async(err, col) => {
+      if (err) {
+        // Collection does not exist. Create collection.
+        try {
+          items = await conn.db(process.env.MONGODB_DB).createCollection('items');
+        } catch (e) {
+          console.error(`Unable to create collection: ${e}`);
+        }
+        // Create an index
+        try {
+          await items.createIndex({ category: 1, description: 'text' });
+          await items.createIndex({ location: '2dsphere' });
+        } catch (e) {
+          console.error(`Unable to create indexes: ${e}`);
+        }
+      } else {
+        items = col;
+      }
+    });
   }
 
-  static addItem(name, quantity, price, picture, description, artistId, artistMoniker, categories) {
+  static addItem(name, quantity, price, picture, description, artistId, artistName, category, location) {
     return items.insertOne({
-      name, quantity, price, picture, description, artistId, artistMoniker, categories
-     });
+      name, quantity, price, picture, description, artistId, artistName, category, location
+    });
   }
 
-  static updateItem(id, name, quantity, price, picture, description, artistId, artistMoniker, categories) {
+  static updateItem(id, name, quantity, price, picture, description, artistId, artistName, category, location) {
     return items.updateOne(
       { _id: ObjectId(id) },
-      { $set: { name, quantity, price, picture, description, artistId, artistMoniker, categories } }
+      { $set: { name, quantity, price, picture, description, artistId, artistName, category, location } }
+    );
+  }
+
+  static updateArtistInfo(artistId, artistName, location) {
+    return items.updateMany(
+      { artistId: ObjectId(artistId) },
+      { $set: { artistName, location } }
     );
   }
 
@@ -31,8 +53,8 @@ class ItemsDAO {
     return items.find({ }).toArray();
   }
 
-  static getItemsByArtist(id) {
-    return items.find({ artistId: ObjectId(id) }).toArray();
+  static getItemsByArtist(artistId) {
+    return items.find({ artistId: ObjectId(artistId) }).toArray();
   }
 
   static deleteItem(id) {
