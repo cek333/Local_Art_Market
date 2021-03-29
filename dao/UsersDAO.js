302dbee6b0f2d7e7ea3763607b4988f1939be329
Cecil <69444825+cek333@users.noreply.github.com>
2021-03-29 12:26:25 -0400

    lookup artist/customer info inorder to construct req.user object for passport/server use

--- dao/UsersDAO.js
+++ dao/UsersDAO.js
@@ -14,12 +14,66 @@ class UsersDAO {
     }
   }
 
-  static getUserById(id) {
-    return users.findOne({ _id: ObjectId(id) });
+  // static getUserById(id) {
+  //   return users.findOne({ _id: ObjectId(id) });
+  // }
+
+  // static getUserByEmail(email, type) {
+  //   return users.findOne({ email, type });
+  // }
+
+  static async getUserById(id) {
+    // Don't know type yet, lookup in both artist and customer
+    const queryPipeline = [
+      { $match: { _id: ObjectId(id) } },
+      {
+        $lookup: {
+          from: 'artists',
+          localField: 'typeId',
+          foreignField: '_id',
+          as: 'artistInfo'
+        }
+      },
+      {
+        $lookup: {
+          from: 'customers',
+          localField: 'typeId',
+          foreignField: '_id',
+          as: 'customerInfo'
+        }
+      }
+    ];
+    return (await users.aggregate(queryPipeline)).next();
   }
 
-  static getUserByEmail(email, type) {
-    return users.findOne({ email, type });
+  static async getUserByEmail(email, type) {
+    if (type === 'artist') {
+      const artistPipeline = [
+        { $match: { email, type } },
+        {
+          $lookup: {
+            from: 'artists',
+            localField: 'typeId',
+            foreignField: '_id',
+            as: 'artistInfo'
+          }
+        }
+      ];
+      return (await users.aggregate(artistPipeline)).next();
+    } else {
+      const customerPipeline = [
+        { $match: { email, type } },
+        {
+          $lookup: {
+            from: 'customers',
+            localField: 'typeId',
+            foreignField: '_id',
+            as: 'customerInfo'
+          }
+        }
+      ];
+      return (await users.aggregate(customerPipeline)).next();
+    }
   }
 
   static addUser(email, password, type, typeId) {
