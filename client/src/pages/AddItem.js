33d0bf4427cf29b5889c2200a5e6f4c08b7f18f5
Cecil <69444825+cek333@users.noreply.github.com>
2021-04-02 22:27:09 -0400

    change categories (to match etsy); changed to controlled form

--- client/src/pages/AddItem.js
+++ client/src/pages/AddItem.js
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import Radio from '../components/Radio';
 import API from '../utils/API';
 
 function AddItem(props) {
+  const CATEGORIES = ['prints', 'drawing & illustration', 'painting', 'photography', 'sculpture',
+    'dolls & miniatures', 'textiles', 'mixed media & collage', 'cermaics', 'glass art', 'other'];
   const [ errorMsg, setErrorMsg ] = useState('');
   const [ successMsg, setSuccessMsg ] = useState('');
+  const [ itemProperties, setItemProperties ] = useState({
+    name: '', quantity: 1, price: '25.00', picture: 'https://via.placeholder.com/200',
+    description: '', category: 'other'
+  });
   const history = useHistory();
   // const { type: propsType, updateUser: propsUpdateUser } = props;
 
@@ -13,27 +20,21 @@ function AddItem(props) {
     setSuccessMsg('');
   }
 
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+    setItemProperties({ ...itemProperties, [name]: value });
+  }
+
   function handleSubmit(evt) {
     evt.preventDefault();
     clearMessages();
-    const form = evt.target;
-    // https://stackoverflow.com/questions/23139876/getting-all-form-values-by-javascript
-    let reqBody = {};
-    Object.keys(form.elements).forEach(key => {
-        let element = form.elements[key];
-        if (element.type !== 'submit') {
-          if (element.type === 'select-multiple') {
-            const selAsArray = [];
-            for (let idx=0; idx < element.selectedOptions.length; idx++) {
-              selAsArray.push(element.selectedOptions[idx].value);
-            }
-            reqBody[element.name] = selAsArray;
-          } else {
-            reqBody[element.name] = element.value;
-          }
-        }
-    });
-    console.log('AddItem:', reqBody);
+    console.log('AddItem handleSubmit:', itemProperties);
+    const reqBody = {...itemProperties};
+    // Convert quantity/amt from string to numbers
+    if (reqBody.quantity !== 'unlimited') {
+      reqBody.quantity = Number(reqBody.quantity);
+    }
+    reqBody.price = Number(reqBody.price);
     API.addItem(reqBody, (res) => {
       if (res.status) {
         setSuccessMsg(res.message);
@@ -46,26 +47,30 @@ function AddItem(props) {
   return (
     <div className='box box_center'>
       <form className='box' onSubmit={handleSubmit}>
-        <label htmlFor='name'>Name</label>
-        <input type='text' name='name' id='name'
-              placeholder='name' required />
-        <label htmlFor='quantity'>Quantity</label>
-        <input type='number' name='quantity' id='quantity'
-              placeholder='1' required min='0' />
+        <label htmlFor='name'>Item Name</label>
+        <input type='text' name='name' id='name' value={itemProperties.name}
+              placeholder='name' onChange={handleChange} required />
+        <label htmlFor='quantity'>Quantity (enter number OR 'unlimited')</label>
+        <input type='text' name='quantity' id='quantity' value={itemProperties.quantity}
+              onChange={handleChange} required />
         <label htmlFor='price'>Price</label>
-        <input type='number' name='price' id='price'
-              placeholder='25.00' required min='0' />
+        <input type='text' name='price' id='price' value={itemProperties.price}
+              onChange={handleChange} required />
         <label htmlFor='description'>Description</label>
-        <textarea name='description' id='description'
-              placeholder='Description' required />
-        <label htmlFor="categories">Category</label>
-        <select id="categories" name="categories" multiple required>
-          <option value="portrait">portrait</option>
-          <option value="abstract">abstract</option>
-          <option value="photography">photography</option>
-          <option value="sculpture">sculpture</option>
-          <option value="painting">painting</option>
-        </select> 
+        <textarea name='description' id='description' value={itemProperties.description}
+              placeholder='Description' onChange={handleChange} required />
+        <fieldset>
+          <legend>Categories</legend>
+          {CATEGORIES.map(category => {
+            return (
+              <React.Fragment key={category}>
+                <Radio id={category} name='category' value={category}
+                  onChange={handleChange} selected={itemProperties.category} required />
+                <label htmlFor={category}> {category}</label><br />
+              </React.Fragment>
+            );
+          })}
+        </fieldset>
         <button type='submit'>Add Item</button>
         <p className='successMsg'>{successMsg}</p>
         <p className='errorMsg'>{errorMsg}</p>
