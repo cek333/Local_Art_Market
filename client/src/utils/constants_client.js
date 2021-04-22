6cc7dc4911e7d60fd518cb88d557c8e3c6c0c352
Cecil <69444825+cek333@users.noreply.github.com>
2021-04-22 14:49:38 -0400

    add custom category

--- client/src/utils/constants_client.js
+++ client/src/utils/constants_client.js
@@ -1,6 +1,6 @@
 const constants = {
   CATEGORIES: ['prints', 'drawing & illustration', 'painting', 'photography', 'sculpture',
-'dolls & miniatures', 'textiles', 'mixed media & collage', 'cermaics', 'glass art', 'other'],
+'dolls & miniatures', 'textiles', 'mixed media & collage', 'cermaics', 'glass art', 'custom', 'other'],
   PRICE_LABEL_MAP: {
     0: '$0 to $24.99',
     25: '$25.00 to $49.99',
